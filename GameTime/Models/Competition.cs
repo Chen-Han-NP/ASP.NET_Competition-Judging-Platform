82fd8b467aa51a9c9bcbe173ba1049d2ff1fe9a9
leclerc162003 <S10203330@connect.np.edu.sg>
2021-06-26 23:59:42 +0800

    Create Model for Competition

--- GameTime/Models/Competition.cs
+++ GameTime/Models/Competition.cs
@@ -8,30 +8,30 @@ namespace GameTime.Models
 {
     public class Competition
     {
-        //[Required]
-        //[Display(Name = "Competition ID")]
-        //public int competitionID { get; set; }
+        [Required]
+        [Display(Name = "Competition ID")]
+        public int competitionID { get; set; }
 
-        //[Required(ErrorMessage = "Area of Interest ID is required.")]
-        //[Display(Name = "Area Of Interest ID")]
-        //public int areaInterestID { get; set; }
+        [Required(ErrorMessage = "Area of Interest ID is required.")]
+        [Display(Name = "Area Of Interest ID")]
+        public int areaInterestID { get; set; }
 
-        //[Required(ErrorMessage = "Name is required.")]
-        //[StringLength(255, ErrorMessage = "Competition name cannot exceed 255 characters.")]
-        //[Display(Name = "Competition Name")]
-        //public string competitionName { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(255, ErrorMessage = "Competition name cannot exceed 255 characters.")]
+        [Display(Name = "Competition Name")]
+        public string competitionName { get; set; }
 
-        //[Display(Name = "Start Date")]
-        //[DataType(DataType.Date)]
-        //public DateTime startDate { get; set; }
+        [Display(Name = "Start Date")]
+        [DataType(DataType.Date)]
+        public DateTime startDate { get; set; }
 
-        //[Display(Name = "End Date")]
-        //[DataType(DataType.Date)]
-        //public DateTime endDate { get; set; }
+        [Display(Name = "End Date")]
+        [DataType(DataType.Date)]
+        public DateTime endDate { get; set; }
 
-        //[Display(Name = "Result Released Date")]
-        //[DataType(DataType.Date)]
-        //public DateTime resultReleasedDate { get; set; }
+        [Display(Name = "Result Released Date")]
+        [DataType(DataType.Date)]
+        public DateTime resultReleasedDate { get; set; }
 
     }
 }
