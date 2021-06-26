2fb709f39abc9cdc55f763dbff62fd586ce6ef45
leclerc162003 <S10203330@connect.np.edu.sg>
2021-06-26 23:58:34 +0800

    Create Model for Area of Interest

---
+++ GameTime/Models/AreaOfInterest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameTime.Models
+{
+    public class AreaOfInterest
+    {
+
+        [Required]
+        [Display(Name = "Area Of Interest ID")]
+        public int areaInterestID { get; set; }
+
+
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, ErrorMessage = "Area of Interest name cannot exceed 50 characters.")]
+        [Display(Name = "Area of Interest Name")]
+        public string Name { get; set; }
+
+
+
+    }
+}
