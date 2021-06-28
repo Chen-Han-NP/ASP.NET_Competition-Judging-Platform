9d0795ed12fd546e816973965751c4cffbceb2be
Chen Han <s10202961@connect.np.edu.sg>
2021-06-28 15:17:50 +0800

    Added Comment.cs Model

---
+++ GameTime/Models/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace GameTime.Models
+{
+    public class Comment
+    {
+        public string CommentID { get; set; }
+
+        [StringLength(255, ErrorMessage = "Your comment must not exceed 255 characters!")]
+        public string Description { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime DateTimePosted { get; set; }
+    }
+}
