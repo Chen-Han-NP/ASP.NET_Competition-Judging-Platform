de31a3c4b119b465ca5481f50f54f0794cfe266d
TzeSiong <s10204388@connect.np.edu.sg>
2021-06-28 16:46:41 +0800

    Added Login and CompetitorSignUp model.

---
+++ GameTime/Models/CompetitorSignUp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameTime.Models
+{
+    public class CompetitorSignUp
+    {
+        [Required]
+        [Display(Name = "First name:")]
+        [StringLength(50, ErrorMessage = "Cannot Exceed 50 characters.")]
+        public string FName { get; set; }
+
+        [Required]
+        [Display(Name = "Last name:")]
+        [StringLength(50, ErrorMessage = "Cannot Exceed 50 characters.")]
+        public string LName { get; set; }
+
+        [Required]
+        [Display(Name = "Nickname:")]
+        [StringLength(50, ErrorMessage = "Cannot Exceed 50 characters.")]
+        public string Nickname { get; set; }
+
+        [Required]
+        [Display(Name = "Email Address:")]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [Display(Name = "Password:")]
+        public string Password { get; set; }
+    }
+}
---
+++ GameTime/Models/Login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameTime.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "Email is required.")]
+        [Display(Name = "Email Address:")]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [Display(Name = "Password:")]
+        public string Password { get; set; }
+    }
+}
