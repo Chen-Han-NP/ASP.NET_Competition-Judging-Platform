1a67224a256bd3535632377c129b5c082a8d69ec
leclerc162003 <S10203330@connect.np.edu.sg>
2021-06-29 23:26:00 +0800

    Creating DAL folder and adding AreaOfInterestDAL file

--- GameTime/Controllers/AreaOfInterestController.cs
+++ GameTime/Controllers/AreaOfInterestController.cs
@@ -1,18 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using GameTime.Models;
+using GameTime.SQ
 
 namespace GameTime.Controllers
 {
     public class AreaOfInterestController : Controller
     {
-        public IActionResult Index()
+        //create form default page
+        public IActionResult Create()
         {
-            return View();
+            // Stop accessing the action if not logged in
+            // or account not in the "Administrator" role
+            // ...need to do 
+
+            //prepare data for view
+            ViewData["ShowResult"] = false;
+
+            //create Area of Interest object
+            AreaOfInterest aoi = new AreaOfInterest();
+            
+            return View(aoi);
+        }
+
+        [HttpPost]
+        public IActionResult Create(AreaOfInterest aoi)
+        {
+            // The aoi object contains user inputs from view
+            if (!ModelState.IsValid) // validation fails
+            {
+                return View(aoi); // returns the view with errors
+            }
+            if (ModelState.IsValid)
+            {
+                //Add staff record to database
+                aoi.AreaInterestID = aoi.Add(staff);
+                //Redirect user to Staff/Index view
+                return RedirectToAction("Index");
+            }
+
+            //generate Area of Interest ID
+
         }
     }
 }
---
+++ GameTime/DAL/AreaOfInterestDAL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameTime.DAL
+{
+    public class AreaOfInterestDAL
+    {
+    }
+}
