81931f1dee0deee292cf44ea419c3fb5734da158
TzeSiong <s10204388@connect.np.edu.sg>
2021-07-05 22:58:25 +0800

    Package 2: Added Sign Up UI and Competitor Sign Up UI

---
+++ GameTime/Controllers/SignUpController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GameTime.Models;
+
+namespace GameTime.Controllers
+{
+    public class SignUpController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public ActionResult CompetitorSignUp()
+        {
+            return View();
+        }
+        public ActionResult JudgeSignUp()
+        {
+            return View();
+        }
+    }
+}
