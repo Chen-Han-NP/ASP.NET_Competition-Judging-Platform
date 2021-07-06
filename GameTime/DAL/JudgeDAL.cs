14993a42355f34e4acb3bf71ca690392b6d8e55b
Xavier Teo <s10203441@connect.np.edu.sg>
2021-07-06 16:02:54 +0800

    Add JudgeDAL and Judge Email validation

--- GameTime/DAL/JudgeDAL.cs
+++ GameTime/DAL/JudgeDAL.cs
@@ -30,7 +30,53 @@ namespace GameTime.DAL
 
         public int Add(JudgeSignUp judgeSignUp)
         {
+            //Create a SqlCommand object from connection object
+            SqlCommand cmd = conn.CreateCommand();
 
+            cmd.CommandText = @"INSERT INTO Judge (JudgeName, Salutation, AreaInterestID, EmailAddr, Password)
+                                OUTPUT INSERTED.JudgeID
+                                VALUES(@judgeName, @salutation, @areaInterestID, @emailAddr, @password)";
+            cmd.Parameters.AddWithValue("@judgeName", judgeSignUp.JudgeName);
+            cmd.Parameters.AddWithValue("@salutation", judgeSignUp.Salutation);
+            cmd.Parameters.AddWithValue("@areaInterestID", judgeSignUp.AreaInterestID);
+            cmd.Parameters.AddWithValue("@emailAddr", judgeSignUp.EmailAddr);
+            cmd.Parameters.AddWithValue("@password", judgeSignUp.Password);
+
+            conn.Open();
+
+            judgeSignUp.JudgeID = (int)cmd.ExecuteScalar();
+
+            conn.Close();
+
+            return judgeSignUp.JudgeID;
+        }
+        public bool isEmailExists(string email, int judgeID)
+        {
+            bool emailFound = false;
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = @"SELECT JudgeID FROM Judge
+                                WHERE EmailAddr=@selectedEmail";
+            cmd.Parameters.AddWithValue("@selectedEmail", email);
+
+            conn.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            { //Records found
+                while (reader.Read())
+                {
+                    if (reader.GetInt32(0) != judgeID)
+                        //The email address is used by another staff
+                        emailFound = true;
+                }
+            }
+            else
+            { //No record
+                emailFound = false; // The email address given does not exist
+            }
+            reader.Close();
+            conn.Close();
+
+            return (emailFound);
         }
     }
 }
--- GameTime/Models/ValidateEmailExists.cs
+++ GameTime/Models/ValidateEmailExists.cs
@@ -10,13 +10,20 @@ namespace GameTime.Models
     public class ValidateEmailExists : ValidationAttribute
     {
         private CompetitorDAL competitorContext = new CompetitorDAL();
+        private JudgeDAL judgeContext = new JudgeDAL();
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             string email = Convert.ToString(value);
             CompetitorSignUp competitor = (CompetitorSignUp)validationContext.ObjectInstance;
             int competitorID = competitor.CompetitorID;
+            JudgeSignUp judge = (JudgeSignUp)validationContext.ObjectInstance;
+            int judgeID = judge.JudgeID;
 
             if (competitorContext.isEmailExists(email, competitorID))
+                // validation failed
+                return new ValidationResult
+                ("Email address already exists!");
+            else if (judgeContext.isEmailExists(email, judgeID))
                 // validation failed
                 return new ValidationResult
                 ("Email address already exists!");
