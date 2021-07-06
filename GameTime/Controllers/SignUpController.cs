300246b14d7d0d23bd7d5f707d4f2feee963470e
Xavier Teo <s10203441@connect.np.edu.sg>
2021-07-06 17:29:18 +0800

    Add AOI dropdown and JudgeSIgnUp in SignUpController

--- GameTime/Controllers/SignUpController.cs
+++ GameTime/Controllers/SignUpController.cs
@@ -5,12 +5,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using GameTime.Models;
 using GameTime.DAL;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace GameTime.Controllers
 {
     public class SignUpController : Controller
     {
         private CompetitorDAL competitorContext = new CompetitorDAL();
+        private JudgeDAL judgeContext = new JudgeDAL();
         public ActionResult Index()
         {
             return View("Index");
@@ -34,9 +36,42 @@ namespace GameTime.Controllers
                 return View(competitor);
             }
         }
-        public ActionResult CreateJudge(JudgeSignUp judge)
+
+        public ActionResult JudgeSignUp()
         {
+            ViewData["GetAOI"] = GetAOI();
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult JudgeSignUp(JudgeSignUp judge)
+        {
+            if (ModelState.IsValid)
+            {
+                judge.JudgeID = judgeContext.Add(judge);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View(judge);
+            }
+        }
+
+        private List<SelectListItem> GetAOI()
+        {
+            AreaOfInterestDAL AOIcontext = new AreaOfInterestDAL();
+            List<AreaOfInterest> AOI = AOIcontext.GetAreaOfInterests();
+            List<SelectListItem> SelectList = new List<SelectListItem>();
+            for (int i = 0; i < AOI.Count(); i++)
+            {
+                SelectList.Add(new SelectListItem
+                {
+                    Value = i.ToString(),
+                    Text = AOI[i].ToString()
+                });
+            }
+            ViewData["AOIList"] = SelectList;
+            return SelectList;
         }
     }
 }
