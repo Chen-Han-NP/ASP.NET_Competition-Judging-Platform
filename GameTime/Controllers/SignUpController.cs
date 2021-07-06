758eb834594233ea04c349dc50e52a4ffb302ca6
TzeSiong <s10204388@connect.np.edu.sg>
2021-07-06 14:52:54 +0800

    Package 2: Updated SignUpController and CompetitorSignUp to add data to database.

--- GameTime/Controllers/SignUpController.cs
+++ GameTime/Controllers/SignUpController.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GameTime.Models;
+using GameTime.DAL;
 
 namespace GameTime.Controllers
 {
     public class SignUpController : Controller
     {
+        private CompetitorDAL competitorContext = new CompetitorDAL();
         public ActionResult Index()
         {
             return View("Index");
@@ -17,6 +19,20 @@ namespace GameTime.Controllers
         public ActionResult CompetitorSignUp()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateCompetitor(CompetitorSignUp competitor)
+        {
+            if (ModelState.IsValid)
+            {
+                competitor.CompetitorID = competitorContext.Add(competitor);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View(competitor);
+            }
         }
         public ActionResult JudgeSignUp()
         {
