e64ddd8f2f00e17f6f5aa184380a00a9c54be85f
KatachiX <Katachix@gmail.com>
2021-07-10 13:36:56 +0800

    Add Criteria Model

---
+++ GameTime/Models/Criteria.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameTime.Models
+{
+    public class Criteria
+    {
+        [Required]
+        public int CriteriaID { get; set; }
+
+        [Required]
+        public int CompetitionID { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Character name can be up to 50 characters.")]
+        public string CriteriaName { get; set; }
+
+        [Required]
+        [Range(1,100, ErrorMessage = "Weightage can only be from 1 to 100")]
+        public int Weightage { get; set; }
+
+    }
+}
