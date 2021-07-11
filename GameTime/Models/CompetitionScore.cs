e9ab1f81ae6f8422a0ed5c461fa92917da935be9
KatachiX <Katachix@gmail.com>
2021-07-11 13:49:31 +0800

    Create Advance feature CompetitionScore model

---
+++ GameTime/Models/CompetitionScore.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameTime.Models
+{
+    public class CompetitionScore
+    {
+        [Required]
+        public int CriteriaID { get; set; }
+
+        [Required]
+        public int CompetitorID { get; set; }
+
+        [Required]
+        public int CompetitionID { get; set; }
+
+        [Required]
+        [Range(0,10, ErrorMessage = "Score can only range from 0 to 10")]
+        public int Score { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd hh:mm}", ApplyFormatInEditMode = true)]
+        public DateTime? DateTimeLastEdit { get; set; }
+    }
+}
