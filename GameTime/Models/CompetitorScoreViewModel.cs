cd9296a445a910cb5552fc6bfc9dc5c0dabe71d9
KatachiX <Katachix@gmail.com>
2021-08-01 16:35:24 +0800

    Update UI for Judge

--- GameTime/Models/CompetitorScoreViewModel.cs
+++ GameTime/Models/CompetitorScoreViewModel.cs
@@ -9,26 +9,33 @@ namespace GameTime.Models
 {
     public class CompetitorScoreViewModel
     {
+        [Display(Name ="Competitior ID")]
         public int CompetitorID { get; set; }
 
+        [Display(Name ="Competition ID")]
         public int CompetitionID { get; set; }
 
+        [Display(Name ="Competitor Name")]
         public string CompetitorName { get; set; }
 
         public double Score { get; set; }
 
+        [Display(Name ="File Submitted")]
         public string FileSubmitted { get; set; }
 
         [DataType(DataType.DateTime)]
+        [Display(Name ="Date Time Submitted")]
         public DateTime? DateTimeSubmitted { get; set; }
 
         public string Appeal { get; set; }
 
         [Required]
+        [Display(Name ="Vote Count")]
         public int VoteCount { get; set; }
 
         public int? Ranking { get; set; }
 
+        [Display(Name ="File Upload")]
         public IFormFile FileUpload { get; set; }
     }
 }
--- GameTime/Models/CompetitorSubmissionViewModel.cs
+++ GameTime/Models/CompetitorSubmissionViewModel.cs
@@ -11,28 +11,34 @@ namespace GameTime.Models
     public class CompetitorSubmissionViewModel
     {
         [Required]
+        [Display(Name = "Competition ID")]
         public int CompetitionId { get; set; }
 
 
         [Required]
+        [Display(Name = "Competitor ID")]
         public int CompetitorId { get; set; }
 
 
         [StringLength(50, ErrorMessage = "Name cannot excceed 50 letters")]
+        [Display(Name = "Competitor Name")]
         public string CompetitorName { get; set; }
 
         [StringLength(5, ErrorMessage = "Salutation cannot excceed 5 letters")]
         public string Salutation { get; set; }
 
         //Type will be changed to iFormFile
+        [Display(Name = "File Submitted")]
         public string FileSubmitted { get; set; }
 
         [DataType(DataType.DateTime)]
+        [Display(Name = "Date Time Submitted")]
         public DateTime? DateTimeSubmitted { get; set; }
 
         public string Appeal { get; set; }
 
         [Required]
+        [Display(Name = "Vote Count")]
         public int VoteCount { get; set; }
 
         public int? Ranking { get; set; }
